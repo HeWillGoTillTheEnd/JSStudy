49489a87a6047c73b86092fb5ce8ae9d6551370a
HeWillGoTillTheEnd <kingofunder@gmail.com>
2023-10-04 15:11:58 +0900

    js-todolist-finish

--- Todo-List/script.js
+++ Todo-List/script.js
@@ -1,14 +1,8 @@
 const todoInput = document.querySelector('#todo-input');
 const todoList = document.querySelector('#todo-list');
 
-const savedTodoList = JSON.parse.localStorage.getItem('saved-items');
-
-
-if(savedTodoList) {
-    for (let i = 0; i < savedTodoList.length; i++) {
-        createTodo(savedTodoList[i])
-    }
-}
+const savedTodoList = JSON.parse(localStorage.getItem('saved-items'));
+const savedWeatherData = JSON.parse(localStorage.getItem('saved-weather'));
 
 const createTodo = function(storageData) {
     let todoContents = todoInput.value;
@@ -21,26 +15,30 @@ const createTodo = function(storageData) {
     
     newBtn.addEventListener('click', () => {
         newLi.classList.toggle('complete');
-        saveItemsFn()
+        saveItemsFn();
     });
 
 
     newLi.addEventListener('dblclick',() => {
-        console.log('더블 클릭');
         newLi.remove();
-    })
+        saveItemsFn();
 
+    });
+    
+
+    if(storageData?.complete){
+        newLi.classList.add('complete');
+    }
     newSpan.textContent = todoContents;
     newLi.appendChild(newBtn);
     newLi.appendChild(newSpan);
     todoList.appendChild(newLi);
     todoInput.value = '';
     saveItemsFn();
-    return todoList;
 }
 
 const keyCodeCheck = function() {
-    if(window.event.keyCode === 13 && todoInput.value !== ''){
+    if(window.event.keyCode === 13 && todoInput.value.trim() !== ''){
         createTodo();
     }
 };
@@ -51,17 +49,78 @@ const deleteAll = function () {
     for (let i = 0; i < liList.length; i++) {
         liList[i].remove();
     }
+    saveItemsFn();
 }
 
-const saveItemsFn = function (todoList) {
+const saveItemsFn = function () {
     const saveItems = [];
     for (let i = 0; i < todoList.children.length; i++) {
         const todoObj = {
-            contents : todoList.children[i].querySelector('span').textContent,
-            complete : todoList.children[i].classList.contains('complete'), 
+            contents: todoList.children[i].querySelector('span').textContent,
+            complete: todoList.children[i].classList.contains('complete'),
         };
         saveItems.push(todoObj);        
     }
-    
-    localStorage.setItem('saved-items',JSON.stringify(saveItems))
+
+    saveItems.length === 0 ? localStorage.removeItem('saved-items') 
+    : localStorage.setItem('saved-items', JSON.stringify(saveItems));
 };
+
+if(savedTodoList) {
+    for (let i = 0; i < savedTodoList.length; i++) {
+        createTodo(savedTodoList[i]);
+    }
+}
+
+const weatherDataActive = function( { location, weather} ) {
+    const weatherMainList = [
+        'Clear',
+        'Clouds',
+        'Drizzle',
+        'Rain',
+        'Snow',
+        'Thunderstorm',
+    ];
+    weather = weatherMainList.includes(weather) ? weather : 'Fog';
+    const locationNameTag = document.querySelector('#location-name-tag');
+    locationNameTag.textContent = location;
+    document.body.style.backgroundImage = `url('./images/${weather}.jpg')`;
+    if (!savedWeatherData || savedWeatherData.location !== location || savedWeatherData. weather !== weather){
+    localStorage.setItem('saved-weather', JSON.stringify({ location, weather }));
+    }
+};
+
+const weatherSearch = function ({ latitude, longitude}) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=`
+    ).then((res) => {
+        return res.json()
+    }).then((json) => {
+        console.log(json.name,json.weather[0].description);
+        const weatherData = {
+            location: json.name,
+            weather: json.weather[0].main
+        }
+        weatherDataActive(weatherData);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+const accessToGeo = function({ coords }) {
+    const { latitude, longitude } = coords;
+    // shorthand property
+    const positionObj = {
+        latitude,
+        longitude, 
+    };
+    weatherSearch(positionObj);
+}
+
+const askForLocation = function() {
+    navigator.geolocation.getCurrentPosition(accessToGeo, (err) => {console.log(err);});
+}
+askForLocation();
+if(savedWeatherData) {
+    weatherDataActive(savedWeatherData);
+ }
